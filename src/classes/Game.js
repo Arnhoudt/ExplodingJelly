f32296e36ff6ccdc153629b7d94d681d3b9c94a3
Fredcyp <frederikcypers1705@gmail.com>
2018-11-21 15:56:32 +0100

    moving jelly

--- src/classes/Game.js
+++ src/classes/Game.js
@@ -27,7 +27,7 @@ class Game extends Phaser.Game {
       physics: {
         default: `arcade`,
         arcade: {
-          gravity: {y: 300},
+          gravity: {y: 0},
           debug: false
         }
       }
